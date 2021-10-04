76b0689917f924cd2027d8f1932139db8a4e9c13
François-Pierre <francoispierrerousseau.44@gmail.com>
2021-10-04 17:53:28 +0200

    Read Excel

--- Cognac-Behourd/Cognac-Behourd/Program.cs
+++ Cognac-Behourd/Cognac-Behourd/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Cognac_Behourd
 {
@@ -6,7 +10,28 @@ namespace Cognac_Behourd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Yo mec \n Veuillez choisir un menu : \n\n 1. Afficher les adhérents \n 2. Ajouter un nouvel adhérent \n 3. Créer une session");
+
+            List<String> datas = new();
+
+            var arrayPath = Directory.GetCurrentDirectory().Split('\\');
+            string path = "";
+
+            for(var i = 0; i < arrayPath.Length; i++)
+            {
+                if(i < arrayPath.Length - 3)
+                {
+                    path += i == 0 ? $"{arrayPath[i]}" : $"\\{arrayPath[i]}";
+                }
+            }
+
+            path = $"{path}\\Files\\test.xlsx";
+
+            using var wbook = new XLWorkbook(path);
+
+            var ws1 = wbook.Worksheet(1);
+            var data = ws1.Cell("A2").GetValue<string>();
+
+
         }
     }
 }

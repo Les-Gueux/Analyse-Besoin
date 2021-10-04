4fd243d37f01161eb6b9d35ed0dd241313eca0c6
MrMarrant <georgesandalous@gmail.com>
2021-10-04 14:34:07 +0200

    ajout des fichiers de classes

--- Cognac-Behourd/Cognac-Behourd/Program.cs
+++ Cognac-Behourd/Cognac-Behourd/Program.cs
@@ -6,7 +6,7 @@ namespace Cognac_Behourd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Yo mec \n Veuillez choisir un menu : \n\n 1. Afficher les adhérents \n 2. Ajouter un nouvel adhérent \n 3. Créer une session");
         }
     }
 }

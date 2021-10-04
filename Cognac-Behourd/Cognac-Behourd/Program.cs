ffaf1b00b812578de3055db2bcbef6882726b47b
MrMarrant <georgesandalous@gmail.com>
2021-10-04 15:58:58 +0200

    [FEATURE] Ajout du fichier excell et update programm

--- Cognac-Behourd/Cognac-Behourd/Program.cs
+++ Cognac-Behourd/Cognac-Behourd/Program.cs
@@ -7,6 +7,18 @@ namespace Cognac_Behourd
         static void Main(string[] args)
         {
             Console.WriteLine("Yo mec \n Veuillez choisir un menu : \n\n 1. Afficher les adhérents \n 2. Ajouter un nouvel adhérent \n 3. Créer une session");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }

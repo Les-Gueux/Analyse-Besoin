edee97c09c95209dcca8cefdd4ec881a59f62156
François-Pierre <francoispierrerousseau.44@gmail.com>
2021-10-05 09:26:38 +0200

    Add Generic Path

--- Cognac-Behourd/Cognac-Behourd/Program.cs
+++ Cognac-Behourd/Cognac-Behourd/Program.cs
@@ -13,24 +13,27 @@ namespace Cognac_Behourd
 
             
             List<String> datas = new();
+            var dirPath = Path.DirectorySeparatorChar;
 
-            var arrayPath = Directory.GetCurrentDirectory().Split('\\');
+            var arrayPath = Directory.GetCurrentDirectory().Split(dirPath);
             string path = "";
 
             for(var i = 0; i < arrayPath.Length; i++)
             {
                 if(i < arrayPath.Length - 3)
                 {
-                    path += i == 0 ? $"{arrayPath[i]}" : $"\\{arrayPath[i]}";
+                    path += i == 0 ? $"{arrayPath[i]}" : $"{dirPath}{arrayPath[i]}";
                 }
             }
 
-            path = $"{path}\\Files\\test.xlsx";
+            path = $"{path}{dirPath}Files{dirPath}test.xlsx";
 
             using var wbook = new XLWorkbook(path);
 
             var ws1 = wbook.Worksheet(1);
             var data = ws1.Cell("A2").GetValue<string>();
+
+            Console.WriteLine(data);
 
             Console.WriteLine("Yo mec \n Veuillez choisir un menu : \n\n 1. Afficher les adhérents \n 2. Ajouter un nouvel adhérent \n 3. Créer une session");
             string choice = Console.ReadLine();

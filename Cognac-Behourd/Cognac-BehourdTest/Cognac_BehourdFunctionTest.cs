d9c053f5ea1685787c7c8d2160f70ae5c21f2a37
Jterom <moret4419044@gmail.com>
2021-10-05 11:34:20 +0200

    modif pour effacer les erreurs

--- Cognac-Behourd/Cognac-BehourdTest/Cognac_BehourdFunctionTest.cs
+++ Cognac-Behourd/Cognac-BehourdTest/Cognac_BehourdFunctionTest.cs
@@ -4,28 +4,31 @@ using System.Collections.Generic;
 
 namespace Cognac_BehourdTest
 {
-    [TestMethod]
-    public void verifier_que_les_equipes_sont_equlibre_en_nombre_quand_les_joueur_sont_paire()
+    [TestClass]
+    public class Cognac_BehourdTest
     {
-        Session s = new Session();
+        [TestMethod]
+        public void verifier_que_les_equipes_sont_equlibre_en_nombre_quand_les_joueur_sont_paire()
+        {
+            Session s = new Session();
 
-        List<Personne> ListePaticipant = new List<Personne>();
+            List<Personne> ListePaticipant = new List<Personne>();
 
-        Personne p = new Personne("Jo", 65, 17);
-        ListePaticipant.Add(p);
+            Personne p = new Personne("Jo", "e", 65, 17);
+            ListePaticipant.Add(p);
 
-        p = new Personne("Yank", 75, 27);
-        ListePaticipant.Add(p);
+            p = new Personne("Yank", "a", 75, 27);
+            ListePaticipant.Add(p);
 
-        p = new Personne("Ber", 70, 20);
-        ListePaticipant.Add(p);
+            p = new Personne("Jack", "b", 52, 16);
+            ListePaticipant.Add(p);
 
-        p = new Personne("Frank", 72, 30);
-        ListePaticipant.Add(p);
+            p = new Personne("Ber", "c", 70, 20);
+            ListePaticipant.Add(p);
 
-        s.creation_equipe(ListePaticipant);
+            s.creation_equipe(ListePaticipant);
 
-        Assert.Equals(s.ListeEquipe[0].ListePersonne.Count, s.ListeEquipe[1].ListePersonne.Count);
+            Assert.Equals(s.ListeEquipe[0].ListePersonne.Count, s.ListeEquipe[1].ListePersonne.Count);
+        }
     }
-}
 }
